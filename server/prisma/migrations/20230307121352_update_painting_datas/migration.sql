a48f754bf72a58287073dffeb3d6782be4745a3c
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-07 14:30:47 +0100

    update : painter's name as unique,optionals values

---
+++ server/prisma/migrations/20230307121352_update_painting_datas/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `painting` MODIFY `image` VARCHAR(191) NULL;
---
+++ server/prisma/migrations/20230307123145_put_date_birth_and_date_death_to_string/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `painters` MODIFY `birthDate` VARCHAR(191) NULL,
+    MODIFY `deathDate` VARCHAR(191) NULL;
