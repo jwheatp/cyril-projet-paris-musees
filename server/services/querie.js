c269a67e37a21177da7f68aa169aeb2f2574f0b9
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-07 13:07:37 +0100

    update : data given in query

--- server/services/querie.js
+++ server/services/querie.js
@@ -4,335 +4,178 @@ export const getPainting = (query) => `
 	nodeQuery(
     filter: {conditions: [{field: "uuid", value: "${query}"}]}
 	) {
-    entities {
-      entityUuid
-      ... on NodeOeuvre {
-        fieldOeuvreAuteurs {
-          entity {
-            fieldAuteurAuteur {
-              entity {
+		entities {
+		entityUuid
+		... on NodeOeuvre {
+			title
+			absolutePath
+			fieldLrefAdlib
+			fieldUrlAlias
+			fieldTitreDeMediation
+			fieldSousTitreDeMediation
+			fieldOeuvreAuteurs {
+			entity {
+				fieldAuteurAuteur {
+				entity {
+					name
+					fieldPipDateNaissance {
+					startPrecision
+					startYear
+					startMonth
+					startDay
+					sort
+					endPrecision
+					endYear
+					endMonth
+					endDay
+					processed
+					}
+					fieldPipLieuNaissance
+					fieldPipDateDeces {
+					startPrecision
+					startYear
+					startMonth
+					startDay
+					sort
+					endPrecision
+					endYear
+					endMonth
+					endDay
+					processed
+					}
+					fieldLieuDeces
+				}
+				}
+				fieldAuteurFonction {
+				entity {
+					name
+				}
+				}
+			}
+			}
+			fieldVisuels {
+			entity {
+				name
+				vignette
+				publicUrl
+			}
+			}
+			fieldDateProduction {
+			startPrecision
+			startYear
+			startMonth
+			startDay
+			sort
+			endPrecision
+			endYear
+			endMonth
+			endDay
+			century
+			processed
+			}
+			fieldOeuvreSiecle {
+			entity {
+				name
+			}
+			}
+			fieldOeuvreTypesObjet {
+			entity {
+				name
 				fieldLrefAdlib
-                name
-                fieldPipDateNaissance {
-                  sort
-                }
-                fieldPipDateDeces {
-                  sort
-                }
-              }
-            }
-            fieldAuteurFonction {
-              entity {
-                name
-              }
-            }
-          }
-        }
-        fieldVisuels {
-          entity {
-            name
-            vignette
-            publicUrl
-          }
-        }
-        fieldDateProduction {
-          startPrecision
-          startYear
-          startMonth
-          startDay
-          sort
-          endPrecision
-          endYear
-          endMonth
-          endDay
-          century
-          processed
-        }
-        fieldOeuvreSiecle {
-           entity {
-            name
-          }
-        }
-        fieldOeuvreTypesObjet {
-          entity {
-            name
-            fieldLrefAdlib
-            entityUuid
-          }
-        }
-        fieldDenominations {
-          entity {
-            name
-          }
-        }
-        fieldMateriauxTechnique{
-          entity {
-            name
-          }
-        }
-        fieldOeuvreDimensions {
-          entity {
-            fieldDimensionPartie {
-              entity {
-                name
-              }
-            }
-            fieldDimensionType {
-              entity {
-                name
-              }
-            }
-            fieldDimensionValeur
-            fieldDimensionUnite {
-             entity {
-                name
-              }
-            }
-          }
-        }
-        fieldOeuvreInscriptions{
-          entity {
-            fieldInscriptionType {
-              entity {
-                name
-              }
-            }
-            fieldInscriptionMarque {
-              value
-            }
-            fieldInscriptionEcriture {
-              entity {
-                name
-              }
-            }
-          }
-        }
-        fieldOeuvreDescriptionIcono {
-          value
-        }
-        fieldCommentaireHistorique {
-          value
+				entityUuid
+			}
+			}
+			fieldDenominations {
+			entity {
+				name
+			}
+			}
+			fieldMateriauxTechnique{
+			entity {
+				name
+			}
+			}
+			fieldOeuvreDimensions {
+			entity {
+				fieldDimensionPartie {
+				entity {
+					name
+				}
+				}
+				fieldDimensionType {
+				entity {
+					name
+				}
+				}
+				fieldDimensionValeur
+				fieldDimensionUnite {
+				entity {
+					name
+				}
+				}
+			}
+			}
+			fieldOeuvreInscriptions{
+			entity {
+				fieldInscriptionType {
+				entity {
+					name
+				}
+				}
+				fieldInscriptionMarque {
+				value
+				}
+				fieldInscriptionEcriture {
+				entity {
+					name
+				}
+				}
+			}
+			}
+			fieldOeuvreDescriptionIcono {
+			value
+			}
+			fieldCommentaireHistorique {
+			value
 
-        }
-        fieldOeuvreThemeRepresente	 {
-          entity {
-            name
-          }
-        }
-        fieldLieuxConcernes {
-          entity {
-            name
-          }
-        }
-        fieldOeuvreNumInventaire
-        fieldOeuvreStyleMouvement {
-          entity {
-            name
-          }
-        }
-        fieldMusee {
-          entity {
-            name
-          }
-        }
-        fieldOeuvreExpose {
-          entity {
-            name
-          }
-        }
-        fieldHdVisuel {
-          entity {
-            fieldMediaImage {
-              entity {
-                url
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`;
-
-export const getPainter = (query) => `
-{
-	nodeQuery(
-    filter: {conditions: [{field: "uuid", value: "${query}"}]}
-	) {
-    entities {
-      entityUuid
-      ... on NodeOeuvre {
-        title
-        absolutePath
-        fieldLrefAdlib
-        fieldUrlAlias
-        fieldTitreDeMediation
-        fieldSousTitreDeMediation
-        fieldOeuvreAuteurs {
-          entity {
-            fieldAuteurAuteur {
-              entity {
-				fieldLrefAdlib
-                name
-                fieldPipDateNaissance {
-                 startPrecision
-                  startYear
-                  startMonth
-                  startDay
-                  sort
-                  endPrecision
-                  endYear
-                  endMonth
-                  endDay
-                  processed
-                }
-                fieldPipLieuNaissance
-                fieldPipDateDeces {
-                 startPrecision
-                  startYear
-                  startMonth
-                  startDay
-                  sort
-                  endPrecision
-                  endYear
-                  endMonth
-                  endDay
-                  processed
-                }
-                 fieldLieuDeces
-              }
-            }
-            fieldAuteurFonction {
-              entity {
-                name
-              }
-            }
-          }
-        }
-        fieldVisuels {
-          entity {
-            name
-            vignette
-            publicUrl
-          }
-        }
-        fieldDateProduction {
-          startPrecision
-          startYear
-          startMonth
-          startDay
-          sort
-          endPrecision
-          endYear
-          endMonth
-          endDay
-          century
-          processed
-        }
-        fieldOeuvreSiecle {
-           entity {
-            name
-          }
-        }
-        fieldOeuvreTypesObjet {
-          entity {
-            name
-            fieldLrefAdlib
-            entityUuid
-          }
-        }
-        fieldDenominations {
-          entity {
-            name
-          }
-        }
-        fieldMateriauxTechnique{
-          entity {
-            name
-          }
-        }
-        fieldOeuvreDimensions {
-          entity {
-            fieldDimensionPartie {
-              entity {
-                name
-              }
-            }
-            fieldDimensionType {
-              entity {
-                name
-              }
-            }
-            fieldDimensionValeur
-            fieldDimensionUnite {
-             entity {
-                name
-              }
-            }
-          }
-        }
-        fieldOeuvreInscriptions{
-          entity {
-            fieldInscriptionType {
-              entity {
-                name
-              }
-            }
-            fieldInscriptionMarque {
-              value
-            }
-            fieldInscriptionEcriture {
-              entity {
-                name
-              }
-            }
-          }
-        }
-        fieldOeuvreDescriptionIcono {
-          value
-        }
-        fieldCommentaireHistorique {
-          value
-
-        }
-        fieldOeuvreThemeRepresente	 {
-          entity {
-            name
-          }
-        }
-        fieldLieuxConcernes {
-          entity {
-            name
-          }
-        }
-        fieldOeuvreNumInventaire
-        fieldOeuvreStyleMouvement {
-          entity {
-            name
-          }
-        }
-        fieldMusee {
-          entity {
-            name
-          }
-        }
-        fieldOeuvreExpose {
-          entity {
-            name
-          }
-        }
-        fieldHdVisuel {
-          entity {
-            fieldMediaImage {
-              entity {
-                url
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+			}
+			fieldOeuvreThemeRepresente	 {
+			entity {
+				name
+			}
+			}
+			fieldLieuxConcernes {
+			entity {
+				name
+			}
+			}
+			fieldOeuvreNumInventaire
+			fieldOeuvreStyleMouvement {
+			entity {
+				name
+			}
+			}
+			fieldMusee {
+			entity {
+				name
+			}
+			}
+			fieldOeuvreExpose {
+			entity {
+				name
+			}
+			}
+			fieldHdVisuel {
+			entity {
+				fieldMediaImage {
+				entity {
+					url
+				}
+				}
+			}
+			}
+		}
+		}
+	}
 }
 `;
