764415e55404124725523ea79cdd7491c0574bda
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-08 18:48:21 +0100

    update : add CSS to quizz and add arrow

--- client/src/components/Quizz.vue
+++ client/src/components/Quizz.vue
@@ -3,28 +3,44 @@ import { getRandomPainters, getRandomPainting } from "@/services/painters";
 </script>
 
 <template>
-	<h1>Quizz ({{ count }}/10)</h1>
+	<section
+		class="d-flex flex-column align-items-center justify-content-around"
+	>
+		<div class="d-flex flex-column align-items-center">
+			<h1>Quizz ({{ count }}/10)</h1>
+			<h2>Qui a peint cette œuvre ?</h2>
+		</div>
+		<div>
+			<img class="img-fluid" :src="painting.image" alt="" />
+		</div>
 
-	<h2>Qui a peint cete œuvre ?</h2>
+		<div class="row">
+			<div
+				class="col-6 ps-4 pe-4"
+				v-for="(painter, idx) in painters"
+				:key="idx"
+			>
+				<button
+					class="answerBtn"
+					:value="painter.id"
+					@click="checkAnswer"
+				>
+					{{ painter.name }}
+				</button>
+			</div>
+		</div>
 
-	<img :src="painting.image" alt="" />
-
-	<div class="row">
-		<div class="col-6" v-for="(painter, idx) in painters" :key="idx">
-			<button :value="painter.id" @click="checkAnswer">
-				{{ painter.name }}
+		<div class="infos d-flex">
+			<p class="infosMessage">{{ message }}</p>
+			<button class="infosBtn" @click="nextRound" v-if="answered">
+				<img src="@/assets/arrow.svg" alt="" />
 			</button>
 		</div>
-	</div>
-
-	<RouterLink to="/game">
-		<button @click="nextRound">Next</button>
-	</RouterLink>
+	</section>
 </template>
 
 <script>
 export default {
-	name: "Quizz",
 	data() {
 		return {
 			painters: null,
@@ -32,14 +48,16 @@ export default {
 			randomPainter: null,
 			count: 1,
 			score: 0,
+			message: "choisissez une réponse",
+			answered: false,
 		};
 	},
 	created() {
-		this.test();
+		this.getDatas();
 	},
 
 	methods: {
-		async test() {
+		async getDatas() {
 			this.painters = await getRandomPainters();
 
 			this.randomPainter =
@@ -52,31 +70,64 @@ export default {
 			this.painting = await getRandomPainting(this.randomPainter);
 
 			console.log(this.painting);
+		},
+		resetAnswerButtons() {
+			const buttons = document.querySelectorAll(".answerBtn");
+			buttons.forEach((button) => {
+				button.disabled = false;
+			});
 
-			const buttons = document.querySelectorAll("button");
 			buttons.forEach((button) => {
 				button.classList.remove("goodAnswer");
 				button.classList.remove("badAnswer");
 			});
 		},
+
+		resetInfos() {
+			this.message = "choisissez une réponse";
+			this.answered = false;
+			const infosMessage = document.querySelector(".infosMessage");
+			infosMessage.classList.remove("infosMessage-goodAnswer");
+			infosMessage.classList.remove("infosMessage-badAnswer");
+		},
 		checkAnswer(e) {
 			console.log(e.target.value);
+			this.answered = true;
+			const buttons = document.querySelectorAll(".answerBtn");
+			buttons.forEach((button) => {
+				button.disabled = true;
+			});
 			if (e.target.value == this.painting.painterId) {
 				console.log("Bonne réponse");
 				e.target.classList.add("goodAnswer");
+				this.message = "Bonne réponse";
+				const infosMessage = document.querySelector(".infosMessage");
+				infosMessage.classList.add("infosMessage-goodAnswer");
 				this.score++;
 			} else {
 				console.log("Mauvaise réponse");
 				e.target.classList.add("badAnswer");
+				const goodAnswer = document.querySelector(
+					`button[value="${this.painting.painterId}"]`
+				);
+				goodAnswer.classList.add("goodAnswer");
+				this.message = "Mauvaise réponse";
+				const infosMessage = document.querySelector(".infosMessage");
+				infosMessage.classList.add("infosMessage-badAnswer");
 			}
 		},
 		nextRound() {
-			// increment count and get new painting
+			this.count++;
 			if (this.count > 10) {
-				this.$router.push("/score");
+				// aller a la page score avec le score en props
+				this.$router.push({
+					name: "score",
+					params: { score: this.score },
+				});
 			} else {
-				this.count++;
-				this.test();
+				this.getDatas();
+				this.resetAnswerButtons();
+				this.resetInfos();
 			}
 		},
 	},
@@ -84,14 +135,63 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "@/assets/_variables.scss";
+@import "node_modules/bootstrap/scss/bootstrap.scss";
 $goodAnswer: #0aa607;
 $badAnswer: #a52853;
 
-.goodAnswer {
-	background-color: $goodAnswer;
-}
+section {
+	height: 100vh;
 
-.badAnswer {
-	background-color: $badAnswer;
+	h1,
+	h2 {
+		margin: 0;
+	}
+
+	h1 {
+		border-bottom: 2px solid $background2;
+	}
+
+	.answerBtn {
+		background-color: #e6e6e6;
+		border: none;
+		border-radius: 5px;
+		margin: 10px 0;
+		width: 100%;
+		height: 80px;
+	}
+
+	.goodAnswer {
+		background-color: $goodAnswer;
+		color: #ffffff;
+	}
+
+	.badAnswer {
+		background-color: $badAnswer;
+		color: #ffffff;
+	}
+
+	.infos {
+		width: 100%;
+		@extend .d-flex, .justify-content-around, .align-items-center;
+		.infosMessage {
+			margin: 0;
+			&-goodAnswer {
+				color: $goodAnswer;
+			}
+
+			&-badAnswer {
+				color: $badAnswer;
+			}
+		}
+		.infosBtn {
+			padding: 0;
+			width: 50px;
+			height: 50px;
+			border: none;
+			border-radius: 5px;
+			background-color: $background2;
+		}
+	}
 }
 </style>
