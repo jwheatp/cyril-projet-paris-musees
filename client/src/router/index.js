2aa583ca1a38e7cbd98154504842fee1945ec176
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-09 12:01:28 +0100

    update : add ScoreView

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import GameView from "@/views/GameView.vue";
+import ScoreView from "@/views/ScoreView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +17,15 @@ const router = createRouter({
 			// route level code-splitting
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import("@/views/GameView.vue"),
+			component: GameView,
+		},
+		{
+			path: "/score/:score",
+			name: "score",
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: ScoreView,
 		},
 	],
 });
---
+++ client/src/views/ScoreView.vue
@@ -0,0 +1,72 @@
+<template>
+	<section class="d-flex flex-column justify-content-between">
+		<div class="mt-5">
+			<h1 class="text-center">Score : {{ score }}</h1>
+			<h2 class="infosScore text-center h4">{{ message }}</h2>
+		</div>
+		<router-link to="/game" class="mb-5">
+			<button class="scoreBtn">Rejouer</button>
+		</router-link>
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			score: undefined,
+			message: null,
+		};
+	},
+	created() {
+		this.score = this.$route.params.score;
+	},
+	mounted() {
+		this.scoreMessage();
+	},
+	methods: {
+		scoreMessage() {
+			const infosScore = document.querySelector(".infosScore");
+			if (0 < this.score && this.score < 5) {
+				this.message = "Pas mal !";
+				infosScore.classList.add("infosScore-bad");
+			} else if (5 <= this.score && this.score < 10) {
+				this.message = "Bravo !";
+				infosScore.classList.add("infosScore-medium");
+			} else if (this.score == 10) {
+				this.message = "Excellent !";
+				infosScore.classList.add("infosScore-good");
+			}
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss">
+$goodScore: #0aa607;
+$mediumScore: orange;
+$badScore: #a52853;
+
+section {
+	height: 100vh;
+
+	.infosScore-bad {
+		color: $badScore;
+	}
+	.infosScore-medium {
+		color: $mediumScore;
+	}
+	.infosScore-good {
+		color: $goodScore;
+	}
+
+	.scoreBtn {
+		background-color: #bababa;
+		border: none;
+		border-radius: 5px;
+		margin: 10px 0;
+		width: 100%;
+		height: 80px;
+	}
+}
+</style>
