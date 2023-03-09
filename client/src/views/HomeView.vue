acf0e360d3dbe76510d6cedca69e2c347957c7e6
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-09 12:17:19 +0100

    update : add CSS to HomeView

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup></script>
 
 <template>
-	<h1>Paris Musée</h1>
+	<section class="d-flex flex-column justify-content-between">
+		<div>
+			<h1 class="text-center mt-5">Paris Musée</h1>
+			<h2 class="text-center h4 mt-5">
+				Teste ta culture sur les oeuvres des plus grands peintres !!!
+			</h2>
+		</div>
 
-	<RouterLink to="/game">
-		<button>Start Game</button>
-	</RouterLink>
+		<RouterLink to="/game" class="mb-5">
+			<button class="startBtn">Start Game</button>
+		</RouterLink>
+	</section>
 </template>
+
+<style scoped lang="scss">
+section {
+	width: 100vw;
+	height: 100vh;
+
+	.startBtn {
+		background-color: #bababa;
+		border: none;
+		border-radius: 5px;
+		margin: 10px 0;
+		width: 100%;
+		height: 80px;
+	}
+}
+</style>
